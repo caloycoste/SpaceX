ff94edd9a4745fc3e9537effea9e54d695b5967f
Carlo Coste <cgcoste@iskolarngbayan.pup.edu.ph>
2021-03-17 17:36:58 +0800

    init

--- webpage/src/components/Navigation.js
+++ webpage/src/components/Navigation.js
@@ -6,42 +6,48 @@ import History from './history'
 function Navigation() {
 
     const styles = {
-        
+
         navBar: {
-            height:80,
-            backgroundColor:'gray',
-            display:'flex'
+            backgroundColor: 'gray',
+            display: 'flex',
+            height: 735,
+            backgroundImage: 'url(https://images.pexels.com/photos/23769/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940)',
+            backgroundAttachment: 'fixed',
+            alignItems: 'center',
+            justifyContent: 'center'
         },
 
         navItems: {
             listStyleType: 'none',
-            display:'flex',
-            alignItems: 'center'
+            display: 'flex',
+            alignItems: 'center',
+            translateX: -100,
         },
 
         navLinks: {
             marginRight: 20,
-            color:'white',
+            color: 'white',
             textDecoration: 'none',
             fontSize: 30,
         }
     }
     return (
-        <div landingPage> 
+        <div style={styles.landingPage}>
             <BrowserRouter>
-            <div style={styles.navBar}>
-            <ul style={styles.navItems}>
-                    <li>
-                        <Link Link to='/' style={styles.navLinks}>Home</Link>
-                    </li>
-                    <li>
-                        <Link Link to='/history' style={styles.navLinks}>History</Link>
-                    </li>
-                    <li>
-                        <Link Link to='/missions' style={styles.navLinks}>Missions</Link>
-                    </li>
-                </ul>
-            </div>
+                <div style={styles.navBar}>
+                    <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/SpaceX-Logo-Xonly.svg/1280px-SpaceX-Logo-Xonly.svg.png' style={{width:400}}/>
+                    <ul style={styles.navItems}>
+                        <li>
+                            <Link Link to='/' style={styles.navLinks}>Home</Link>
+                        </li>
+                        <li>
+                            <Link Link to='/history' style={styles.navLinks}>History</Link>
+                        </li>
+                        <li>
+                            <Link Link to='/missions' style={styles.navLinks}>Missions</Link>
+                        </li>
+                    </ul>
+                </div>
 
                 <div>
                     <Switch>
