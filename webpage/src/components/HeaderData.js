b0ff7607e152570e7dd612a40e759cb0373062ce
Carlo Coste <cgcoste@iskolarngbayan.pup.edu.ph>
2021-03-22 19:57:53 +0800

    init

--- webpage/src/components/HeaderData.js
+++ webpage/src/components/HeaderData.js
@@ -1,5 +1,5 @@
 export const firstObj = {
     welcomeText: 'Welcome to Space X!',
-    welcomeDesc:'This page is not related to Space X.',
+    welcomeDesc:'American aerospace company founded in 2002 that helped usher in the era of commercial spaceflight. It was the first private company to successfully launch and return a spacecraft from Earth orbit and the first to launch a crewed spacecraft and dock it with the International Space Station (ISS). Headquarters are in Hawthorne, California.',
     welcomeImg:'https://i.ytimg.com/vi/JESG7x0X0ek/maxresdefault.jpg'
     }
